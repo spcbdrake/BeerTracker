79cd30df3dcfebc5cea9b9c6460b4f930617fd1c
benjamindrake <spcbdrake@yahoo.com>
2015-10-27 12:14:26 -0400

    tracking beer site

---
+++ src/com/company/Beer.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by benjamindrake on 10/27/15.
+ */
+public class Beer {
+    int id;
+    String name;
+    String type;
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,13 @@ import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Beer> beers = new ArrayList();
         Spark.get(
                 "/",
                 ((request, response) -> {
@@ -21,6 +23,7 @@ public class Main {
                     }
                     HashMap m = new HashMap();
                     m.put("username", username);
+                    m.put("beers", beers);
                     return new ModelAndView(m, "logged-in.html");
                 }),
                 new MustacheTemplateEngine()
@@ -28,9 +31,38 @@ public class Main {
         Spark.post(
                 "/login",
                 ((request, response) -> {
-                    String username = request.params("username");
+                    String username = request.queryParams("username");
                     Session session = request.session();
                     session.attribute("username", username);
+                    response.redirect("/");
+                    return "";
+                })
+        );
+        Spark.post(
+                "/create-beer",
+                ((request, response) -> {
+                    Beer beer = new Beer();
+                    beer.id =beers.size() + 1;
+                    beer.name = request.queryParams("beername");
+                    beer.type = request.queryParams("beertype");
+                    beers.add(beer);
+                    response.redirect("/");
+                    return "";
+                })
+        );
+        Spark.post(
+                "/delete-beer",
+                ((request, response) -> {
+                    String id = request.queryParams("beerid");
+                    try {
+                        int idNum = Integer.valueOf(id);
+                        beers.remove(idNum-1);
+                        for (int i = 0; i < beers.size(); i ++) {
+                            beers.get(i).id = i +1;
+                        }
+                    } catch (Exception e) {
+
+                    }
                     response.redirect("/");
                     return "";
                 })
